42403fc2a4cf0b370b1f362767a899de67c46d29
= <=>
2024-04-07 09:48:24 +0300

    start staking-DepositAndReward

--- src/store/store.sol
+++ src/store/store.sol
@@ -1,15 +1,16 @@
-pragma solidity >=0.8.17;
+// pragma solidity >=0.8.17;
 
-contract Store {
-    uint256 storedValue;
+// contract Store {
+//     uint256 storedValue;
 
-    // Function to set the value
-    function setValue(uint256 newValue) public {
-        storedValue = newValue;
-    }
+//     // Function to set the value
+//     function setValue(uint256 newValue) public {
+//         storedValue = newValue;
+//     }
 
-    // Function to get the value
-    function getValue() public view returns (uint256) {
-        return storedValue;
-    }
-}
+//     // Function to get the value
+//     function getValue() public view returns (uint256) {
+//         return storedValue;
+//     }
+// }
+// 
