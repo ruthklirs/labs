8e38e6434fef9928f4d36b7d926d612095e4b8cb
ruthKLirs <ruthk7300@gmail.com>
2024-04-09 13:06:02 +0300

    finish staking

--- src/staking/staking.sol
+++ src/staking/staking.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-
 contract Reward_4_7 is ERC20 {
     address public owner;
     uint private REWARD_AMOUNT = 1000000; // 1 million reward tokens
@@ -16,46 +14,36 @@ contract Reward_4_7 is ERC20 {
         uint timestamp;
         uint amount;
     }
-    
     // Mapping from user address to array of deposits
     mapping(address => Deposit[]) private userDeposits;
-
     constructor() ERC20("Reward_4_7", "RWD_4_7") {
         owner = msg.sender;
         _mint(msg.sender, REWARD_AMOUNT);
     }
-
     modifier onlyOwner() {
         require(msg.sender == owner, "Not authorized");
         _;
     }
-
     function deposit(uint _amount) external {
         require(_amount > 0, "Amount must be greater than 0");
         transferFrom(msg.sender, address(this), _amount);
-        
         userDeposits[msg.sender].push(Deposit(block.timestamp, _amount));
         TOTAL_DEPOSITS += _amount;
     }
-
 function calcRewards(uint _amount) public view returns (uint) {
     if (TOTAL_DEPOSITS == 0) return 0;
-
     uint rewardPool = (REWARD_AMOUNT * 2*WAD) / 100; // 2% of the reward pool
     uint reward = (_amount * rewardPool) / TOTAL_DEPOSITS;
     return reward;
 }
-
     function withdrawWithReward(uint _amount) external {
         require(_amount > 0, "Amount must be greater than 0");
         uint balanceWithReward = getBalanceWithReward(msg.sender);
         require(balanceWithReward >= _amount, "Insufficient available amount for withdrawal with rewards");
-        
         uint reward = calcRewards(_amount);
         transferFrom(address(this), msg.sender, _amount + reward);
         REWARD_AMOUNT -= reward;
         TOTAL_DEPOSITS -= _amount;
-        
         uint remainingAmount = _amount;
         uint countZero = 0;
         for (uint i = 0; i < userDeposits[msg.sender].length && remainingAmount > 0; i++) {
@@ -68,7 +56,6 @@ function calcRewards(uint _amount) public view returns (uint) {
                 break;
             }
         }
-        
         for (uint j = countZero; j < userDeposits[msg.sender].length - 1; j++) {
             userDeposits[msg.sender][j - countZero] = userDeposits[msg.sender][j];
         }
@@ -76,11 +63,9 @@ function calcRewards(uint _amount) public view returns (uint) {
             userDeposits[msg.sender].pop();
         }
     }
-
     function getBalanceWithReward(address _account) public view returns (uint) {
         uint sum;
         uint latestDate = block.timestamp - 7 days;
-        
         for (uint i = 0; i < userDeposits[_account].length; i++) {
             if (userDeposits[_account][i].timestamp < latestDate) {
                 sum += userDeposits[_account][i].amount;
@@ -88,13 +73,20 @@ function calcRewards(uint _amount) public view returns (uint) {
         }
         return sum;
     }
-
     function getTotalBalance(address _account) public view returns (uint) {
         uint totalBalance;
-        
         for (uint i = 0; i < userDeposits[_account].length; i++) {
             totalBalance += userDeposits[_account][i].amount;
         }
         return totalBalance;
     }
 }
+
+
+
+
+
+
+
+
+
