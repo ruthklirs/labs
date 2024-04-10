96a00cf0bae13895ced16379e061418725e337c1
ruthKLirs <ruthk7300@gmail.com>
2024-04-10 17:07:28 +0300

    staking - test

--- src/staking/staking.sol
+++ src/staking/staking.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "forge-std/console.sol";
+
 contract Reward_4_7 is ERC20 {
     address public owner;
     uint private REWARD_AMOUNT = 1000000; // 1 million reward tokens
     //uint private constant SEVEN_DAYS_IN_SECONDS = 7 days;
     uint private TOTAL_DEPOSITS = 0;
-    uint WAD =10000000000;
+    uint WAD = 10000000000;
+    uint DATE=1750140800;
     // Struct to hold deposit information
     struct Deposit {
         uint timestamp;
@@ -16,37 +19,49 @@ contract Reward_4_7 is ERC20 {
     }
     // Mapping from user address to array of deposits
     mapping(address => Deposit[]) private userDeposits;
+
     constructor() ERC20("Reward_4_7", "RWD_4_7") {
         owner = msg.sender;
-        _mint(msg.sender, REWARD_AMOUNT);
+        _mint(address(this), REWARD_AMOUNT);
     }
+
     modifier onlyOwner() {
         require(msg.sender == owner, "Not authorized");
         _;
     }
+
     function deposit(uint _amount) external {
         require(_amount > 0, "Amount must be greater than 0");
         transferFrom(msg.sender, address(this), _amount);
         userDeposits[msg.sender].push(Deposit(block.timestamp, _amount));
         TOTAL_DEPOSITS += _amount;
     }
-function calcRewards(uint _amount) public view returns (uint) {
-    if (TOTAL_DEPOSITS == 0) return 0;
-    uint rewardPool = (REWARD_AMOUNT * 2*WAD) / 100; // 2% of the reward pool
-    uint reward = (_amount * rewardPool) / TOTAL_DEPOSITS;
-    return reward;
-}
-    function withdrawWithReward(uint _amount) external {
+
+    function calcRewards(uint _amount) public view returns (uint) {
+        if (TOTAL_DEPOSITS == 0) return 0;
+        uint rewardPool = (REWARD_AMOUNT * 2 * WAD) / 100; // 2% of the reward pool
+        uint reward = (_amount * rewardPool) / TOTAL_DEPOSITS;
+        return reward;
+    }
+
+    function withdraw(uint _amount) external {
         require(_amount > 0, "Amount must be greater than 0");
         uint balanceWithReward = getBalanceWithReward(msg.sender);
-        require(balanceWithReward >= _amount, "Insufficient available amount for withdrawal with rewards");
+        require(
+            balanceWithReward >= _amount,
+            "Insufficient available amount for withdrawal with rewards"
+        );
         uint reward = calcRewards(_amount);
         transferFrom(address(this), msg.sender, _amount + reward);
         REWARD_AMOUNT -= reward;
         TOTAL_DEPOSITS -= _amount;
         uint remainingAmount = _amount;
         uint countZero = 0;
-        for (uint i = 0; i < userDeposits[msg.sender].length && remainingAmount > 0; i++) {
+        for (
+            uint i = 0;
+            i < userDeposits[msg.sender].length && remainingAmount > 0;
+            i++
+        ) {
             if (userDeposits[msg.sender][i].amount <= remainingAmount) {
                 remainingAmount -= userDeposits[msg.sender][i].amount;
                 userDeposits[msg.sender][i].amount = 0;
@@ -57,36 +72,33 @@ function calcRewards(uint _amount) public view returns (uint) {
             }
         }
         for (uint j = countZero; j < userDeposits[msg.sender].length - 1; j++) {
-            userDeposits[msg.sender][j - countZero] = userDeposits[msg.sender][j];
+            userDeposits[msg.sender][j - countZero] = userDeposits[msg.sender][
+                j
+            ];
         }
         for (uint j = 0; j < countZero; j++) {
             userDeposits[msg.sender].pop();
         }
     }
+
     function getBalanceWithReward(address _account) public view returns (uint) {
-        uint sum;
-        uint latestDate = block.timestamp - 7 days;
+        uint sum = 0;
+        console.log(block.timestamp);
+        console.log(7 days);
+        uint latestDate = DATE - 7*24*60*60;
         for (uint i = 0; i < userDeposits[_account].length; i++) {
-            if (userDeposits[_account][i].timestamp < latestDate) {
+            if (i< latestDate) {
                 sum += userDeposits[_account][i].amount;
             }
         }
         return sum;
     }
-    function getTotalBalance(address _account) public view returns (uint) {
-        uint totalBalance;
-        for (uint i = 0; i < userDeposits[_account].length; i++) {
-            totalBalance += userDeposits[_account][i].amount;
-        }
-        return totalBalance;
+
+    function getTotalBalance() public view returns (uint) {
+        return balanceOf(msg.sender);
+    }
+
+    function mint(uint ammount) public {
+        _mint(msg.sender, ammount);
     }
 }
-
-
-
-
-
-
-
-
-
---
+++ test/staking/staking.t.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "src/staking/staking.sol";
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+// /root/labs/src/staking/staking.sol
+
+contract TestReward is Test {
+    Reward_4_7 reward;
+
+    function setUp() public {
+        reward = new Reward_4_7();
+    }
+
+    function testOwnerInitiallySet() public {
+        console.log(reward.balanceOf(address(reward)));
+    }
+
+    function testInitialBalance() public {
+        console.log(address(reward).balance);
+        assertEq(
+            reward.balanceOf(address(reward)),
+            1000000,
+            "Initial balance should be 1 million tokens"
+        );
+    }
+
+    function testDeposit() public {
+        uint initialBalance = reward.balanceOf(address(reward));
+        address adr1 = address(1234);
+        vm.startPrank(adr1);
+        reward.mint(200);
+        reward.approve(adr1, 100);
+        reward.deposit(100);
+        uint newBalance = reward.balanceOf(address(reward));
+        assertEq(
+            newBalance - initialBalance,
+            100,
+            "Deposit should increase balance by 100 tokens"
+        );
+        vm.stopPrank();
+    }
+
+    // function testCalcRewards() public {
+    //     uint rewards = reward.calcRewards(100);
+    //     assertEq(rewards, 2000, "Rewards calculation should be accurate");
+    // }
+
+    function testwithdraw() public {
+        address adr1 = address(1234);
+        vm.startPrank(adr1);
+        reward.approve(adr1, 50);
+
+        reward.mint(200);
+        reward.withdraw(50);
+        vm.expectRevert(
+            "Insufficient available amount for withdrawal with rewards"
+        );
+           vm.stopPrank();
+    }
+        // uint initialBalance = reward.balanceOf(address(reward));
+
+        // reward.deposit(100);
+        // uint balanceWithDeposit = reward.balanceOf(address(reward));
+        // uint amountToWithdraw = 50;
+        // reward.withdraw(amountToWithdraw);
+        // uint finalBalance = reward.balanceOf(address(reward));
+        // assertEq(
+        //     finalBalance - initialBalance,
+        //     100,
+        //     "Withdrawal should not affect the initial balance"
+        // );
+        // assertEq(
+        //     finalBalance - balanceWithDeposit,
+        //     -50,
+        //     "Withdrawal should reduce the balance by 50 tokens"
+        // );
+    // }
+
+        function testGetBalanceWithReward() public {
+          address adr2 = address(1234);
+        vm.startPrank(adr2);
+        reward.mint(200);
+        reward.approve(adr2, 100);
+        reward.deposit(100);
+            uint balanceWithReward = reward.getBalanceWithReward(adr2);
+           assertEq(balanceWithReward, 0, "Balance with reward should be accurate");
+        }
+
+    //     function testGetTotalBalance() public {
+    //         reward.deposit(100);
+    //         uint totalBalance = reward.getTotalBalance(address(this));
+    //       assertEq(totalBalance, 100, "Total balance should include deposited amount");
+    //     }
+}
