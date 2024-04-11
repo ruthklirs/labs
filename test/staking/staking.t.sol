daa285f16981c54be9a585ebf87906289cdf5937
ruthKlirs <ruthk7300@gmail.com>
2024-04-11 16:34:12 +0300

    staking - test

--- test/staking/staking.t.sol
+++ test/staking/staking.t.sol
@@ -1,99 +1,107 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
-import "src/staking/staking.sol";
-import "foundry-huff/HuffDeployer.sol";
-import "forge-std/Test.sol";
 import "forge-std/console.sol";
-
-// /root/labs/src/staking/staking.sol
-
-contract TestReward is Test {
-    Reward_4_7 reward;
-
-    function setUp() public {
-        reward = new Reward_4_7();
+import "@hack/staking/MyToken.sol";
+contract Reward_4_7{
+    address public owner;
+    uint public REWARD_AMOUNT ; // 1 million reward tokens
+    uint public TOTAL_DEPOSITS = 0;
+    uint WAD = 10000000000;
+    uint BPS=10000;
+    uint PERCENT;
+    MyToken myToken;
+    // Struct to hold deposit information
+    struct Deposit {
+        uint timestamp;
+        uint amount;
     }
-    //עובד???r
-
-    function testOwnerInitiallySet() public {
-        console.log(reward.balanceOf(address(reward)));
+    // Mapping from user address to array of deposits
+    mapping(address => Deposit[]) private userDeposits;
+    constructor(uint reward,uint percent,address token)  {
+        REWARD_AMOUNT=reward;
+        PERCENT=percent;
+        owner = msg.sender;
+        myToken= MyToken(token);
+        myToken.mint( REWARD_AMOUNT);
     }
-
-    function testInitialBalance() public {
-        console.log(address(reward).balance);
-        assertEq(
-            reward.balanceOf(address(reward)),
-            1000000,
-            "Initial balance should be 1 million tokens"
-        );
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
     }
-
-    function testDeposit() public {
-        uint initialBalance = reward.balanceOf(address(reward));
-        address adr1 = address(1234);
-        vm.startPrank(adr1);
-        reward.mint(200);
-        reward.approve(adr1, 100);
-        reward.deposit(100);
-        uint newBalance = reward.balanceOf(address(reward));
-        assertEq(
-            newBalance - initialBalance,
-            100,
-            "Deposit should increase balance by 100 tokens"
-        );
-        vm.stopPrank();
+    function deposit(uint _amount) external {
+        require(_amount > 0, "Amount must be greater than 0");
+        console.log("address 1" , (msg.sender));
+        myToken.transferFrom(msg.sender, address(myToken), _amount);
+        userDeposits[msg.sender].push(Deposit(block.timestamp, _amount));
+    console.log("sssssssss",msg.sender);
+        TOTAL_DEPOSITS += _amount;
     }
-
-    // function testCalcRewards() public {
-    //     uint rewards = reward.calcRewards(100);
-    //     assertEq(rewards, 2000, "Rewards calculation should be accurate");
-    // }
-
-    function testwithdraw() public {
-        address adr1 = address(1234);
-        vm.startPrank(adr1);
-        reward.approve(adr1, 50);
-
-        reward.mint(200);
-        reward.withdraw(50);
-        vm.expectRevert(
+    function calcRewards(uint _amount) public view returns (uint) {
+        console.log(msg.sender,"calc");
+      if (TOTAL_DEPOSITS == 0) return 0;
+      return  REWARD_AMOUNT*WAD*(PERCENT/100)*(_amount/TOTAL_DEPOSITS)/WAD;
+    }
+    function withdraw(uint _amount) external {
+        require(_amount > 0, "Amount must be greater than 0");
+        uint balanceWithReward = getBalanceWithReward(msg.sender);
+        require(
+            balanceWithReward >= _amount,
             "Insufficient available amount for withdrawal with rewards"
         );
-           vm.stopPrank();
-    }
-        // uint initialBalance = reward.balanceOf(address(reward));
-
-        // reward.deposit(100);
-        // uint balanceWithDeposit = reward.balanceOf(address(reward));
-        // uint amountToWithdraw = 50;
-        // reward.withdraw(amountToWithdraw);
-        // uint finalBalance = reward.balanceOf(address(reward));
-        // assertEq(
-        //     finalBalance - initialBalance,
-        //     100,
-        //     "Withdrawal should not affect the initial balance"
-        // );
-        // assertEq(
-        //     finalBalance - balanceWithDeposit,
-        //     -50,
-        //     "Withdrawal should reduce the balance by 50 tokens"
-        // );
-    // }
-
-        function testGetBalanceWithReward() public {
-          address adr2 = address(1234);
-        vm.startPrank(adr2);
-        reward.mint(200);
-        reward.approve(adr2, 100);
-        reward.deposit(100);
-            uint balanceWithReward = reward.getBalanceWithReward(adr2);
-           assertEq(balanceWithReward, 0, "Balance with reward should be accurate");
+        uint reward = calcRewards(_amount);
+        myToken.transfer(msg.sender, _amount + reward);
+        REWARD_AMOUNT -= reward;
+        TOTAL_DEPOSITS -= _amount;
+        uint remainingAmount = _amount;
+        uint countZero = 0;
+        for (
+            uint i = 0;
+            i < userDeposits[msg.sender].length && remainingAmount > 0;
+            i++
+        ) {
+            if (userDeposits[msg.sender][i].amount <= remainingAmount) {
+                remainingAmount -= userDeposits[msg.sender][i].amount;
+                userDeposits[msg.sender][i].amount = 0;
+                countZero++;
+            } else {
+                userDeposits[msg.sender][i].amount -= remainingAmount;
+                break;
+            }
         }
-
-    //     function testGetTotalBalance() public {
-    //         reward.deposit(100);
-    //         uint totalBalance = reward.getTotalBalance(address(this));
-    //       assertEq(totalBalance, 100, "Total balance should include deposited amount");
-    //     }
+        for (uint j = countZero; j < userDeposits[msg.sender].length - 1; j++) {
+            userDeposits[msg.sender][j - countZero] = userDeposits[msg.sender][
+                j
+            ];
+        }
+        for (uint j = 0; j < countZero; j++) {
+            userDeposits[msg.sender].pop();
+        }
+    }
+    function getBalanceWithReward(address _account) public view returns (uint) {
+        uint sum = 0;
+      console.log("hrrrrrrrrru", _account);
+        //console.log(7 days);
+        uint latestDate = block.timestamp - 7 days;
+        for (uint i = 0; i < userDeposits[_account].length; i++) {
+            console.log("@@@",userDeposits[_account][i].amount);
+            if (userDeposits[_account][i].timestamp< latestDate) {
+                sum += userDeposits[_account][i].amount;
+            }
+        }
+        return sum;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
