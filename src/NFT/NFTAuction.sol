5d6a4421bace18ac197ff75eba1724675d32965e
ruthKlirs <ruthk7300@gmail.com>
2024-05-27 15:45:00 +0300

    dafny changes

--- src/NFT/NFTAuction.sol
+++ src/NFT/NFTAuction.sol
@@ -1,124 +1,124 @@
-pragma solidity ^0.8.0;
+// pragma solidity ^0.8.0;
 
-import '../../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol';
-import "./myToken.sol";
-import "forge-std/console.sol";
+// import '../../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol';
+// import "./myToken.sol";
+// import "forge-std/console.sol";
 
-contract NFTAuction {
-    address private owner;
-    ERC721 public nftToken;
-    uint public startingBid;
-    uint public endingBid;
-    uint public startingPrice;
-    uint public tokenId;
-    bool public started;
+// contract NFTAuction {
+//     address private owner;
+//     ERC721 public nftToken;
+//     uint public startingBid;
+//     uint public endingBid;
+//     uint public startingPrice;
+//     uint public tokenId;
+//     bool public started;
 
-    struct Bidder {
-        address _address;
-        uint256 amount;
-    }
+//     struct Bidder {
+//         address _address;
+//         uint256 amount;
+//     }
 
-    struct BidState {
-        bool isExist;
-        uint256 amount;
-    }
+//     struct BidState {
+//         bool isExist;
+//         uint256 amount;
+//     }
 
-    // bidder balances
-    mapping(address => BidState) public balances;
-    Bidder[] public maxStack;
+//     // bidder balances
+//     mapping(address => BidState) public balances;
+//     Bidder[] public maxStack;
 
-    constructor() {
-        owner = msg.sender;
-    }
+//     constructor() {
+//         owner = msg.sender;
+//     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not authorized");
-        _;
-    }
+//     modifier onlyOwner() {
+//         require(msg.sender == owner, "Not authorized");
+//         _;
+//     }
 
-    function start(address _nftToken, uint _endingBid, uint _startingPrice, uint _tokenId) external onlyOwner {
-        nftToken = ERC721(_nftToken);
-        endingBid = _endingBid;
-        startingPrice = _startingPrice;
-        tokenId = _tokenId;
-        nftToken.transferFrom(owner, address(this), tokenId);
-        started = true;
-    }
+//     function start(address _nftToken, uint _endingBid, uint _startingPrice, uint _tokenId) external onlyOwner {
+//         nftToken = ERC721(_nftToken);
+//         endingBid = _endingBid;
+//         startingPrice = _startingPrice;
+//         tokenId = _tokenId;
+//         nftToken.transferFrom(owner, address(this), tokenId);
+//         started = true;
+//     }
 
-    function addToBalances(address _addr, BidState memory obj) internal {
-        if (balances[_addr].isExist) {
-            payable(msg.sender).transfer(balances[_addr].amount);
-        }
-        balances[_addr] = obj;
-    }
+//     function addToBalances(address _addr, BidState memory obj) internal {
+//         if (balances[_addr].isExist) {
+//             payable(msg.sender).transfer(balances[_addr].amount);
+//         }
+//         balances[_addr] = obj;
+//     }
 
-    function getAddressValue(address _addr) public view returns (bool) {
-        return balances[_addr].isExist;
-    }
+//     function getAddressValue(address _addr) public view returns (bool) {
+//         return balances[_addr].isExist;
+//     }
 
-    function updateMax() internal {
-        while (maxStack.length > 0 && !balances[maxStack[maxStack.length - 1]._address].isExist) {
-            maxStack.pop();
-        }
-    }
+//     function updateMax() internal {
+//         while (maxStack.length > 0 && !balances[maxStack[maxStack.length - 1]._address].isExist) {
+//             maxStack.pop();
+//         }
+//     }
 
-    function addQuote() public payable {
-        console.log("user", msg.sender, "amount", msg.value);
-        require(started && block.timestamp < endingBid, "the Auction is closed");
+//     function addQuote() public payable {
+//         console.log("user", msg.sender, "amount", msg.value);
+//         require(started && block.timestamp < endingBid, "the Auction is closed");
         
-        updateMax();
-        if (maxStack.length > 1) {
-            console.log("zzz");
-            require(msg.value > maxStack[maxStack.length - 1].amount, "the price is too low");
-        } else {
-            require(msg.value > startingPrice, "the price is too low");
-        }
+//         updateMax();
+//         if (maxStack.length > 1) {
+//             console.log("zzz");
+//             require(msg.value > maxStack[maxStack.length - 1].amount, "the price is too low");
+//         } else {
+//             require(msg.value > startingPrice, "the price is too low");
+//         }
 
-        Bidder memory newObj = Bidder(address(msg.sender), msg.value);
-        maxStack.push(newObj);
-        BidState memory newObj2 = BidState(true, msg.value);
+//         Bidder memory newObj = Bidder(address(msg.sender), msg.value);
+//         maxStack.push(newObj);
+//         BidState memory newObj2 = BidState(true, msg.value);
         
-        addToBalances(address(msg.sender), newObj2);
-        console.log("userrr", msg.sender, "amount", msg.value);
-    }
+//         addToBalances(address(msg.sender), newObj2);
+//         console.log("userrr", msg.sender, "amount", msg.value);
+//     }
 
-    function cancelQuote() public {
-        require(started && block.timestamp < endingBid, "the Auction is closed");
-        require(balances[address(msg.sender)].isExist, "you are not exist");
-        balances[address(msg.sender)].isExist = false;
-        payable(msg.sender).transfer(balances[address(msg.sender)].amount);
-    }
+//     function cancelQuote() public {
+//         require(started && block.timestamp < endingBid, "the Auction is closed");
+//         require(balances[address(msg.sender)].isExist, "you are not exist");
+//         balances[address(msg.sender)].isExist = false;
+//         payable(msg.sender).transfer(balances[address(msg.sender)].amount);
+//     }
 
-    function returnQuotes() public {
-        require(block.timestamp > endingBid, "the Auction is active");
-        updateMax();
-        for (uint i = 0; i < maxStack.length - 1; i++) {
-            payable(address(maxStack[i]._address)).transfer(maxStack[i].amount);
-        }
-    }
+//     function returnQuotes() public {
+//         require(block.timestamp > endingBid, "the Auction is active");
+//         updateMax();
+//         for (uint i = 0; i < maxStack.length - 1; i++) {
+//             payable(address(maxStack[i]._address)).transfer(maxStack[i].amount);
+//         }
+//     }
 
-    function getMaxStack() external view returns (address, uint256) {
-        uint index = maxStack.length - 1;
-        require(index < maxStack.length, "Index out of bounds");
-        return (maxStack[index]._address, maxStack[index].amount);
-    }
+//     function getMaxStack() external view returns (address, uint256) {
+//         uint index = maxStack.length - 1;
+//         require(index < maxStack.length, "Index out of bounds");
+//         return (maxStack[index]._address, maxStack[index].amount);
+//     }
 
-    function getIsExist() external view returns (bool) {
-        //require(index < maxStack.length, "Index out of bounds");
-        return (balances[msg.sender].isExist);
-    }
+//     function getIsExist() external view returns (bool) {
+//         //require(index < maxStack.length, "Index out of bounds");
+//         return (balances[msg.sender].isExist);
+//     }
 
-    function end() onlyOwner external {
-        require(block.timestamp > endingBid, "the Auction is active");
-        if (maxStack.length < 1) {
-            nftToken.transferFrom(address(this), owner, tokenId);
-        } else {
-            uint amount = maxStack[maxStack.length - 1].amount;
-            payable(address(owner)).transfer(amount);
-            address bidderAddress = maxStack[maxStack.length - 1]._address;
-            nftToken.transferFrom(address(this), bidderAddress, tokenId);
-            returnQuotes();
-            started = false;
-        }
-    }
-}
+//     function end() onlyOwner external {
+//         require(block.timestamp > endingBid, "the Auction is active");
+//         if (maxStack.length < 1) {
+//             nftToken.transferFrom(address(this), owner, tokenId);
+//         } else {
+//             uint amount = maxStack[maxStack.length - 1].amount;
+//             payable(address(owner)).transfer(amount);
+//             address bidderAddress = maxStack[maxStack.length - 1]._address;
+//             nftToken.transferFrom(address(this), bidderAddress, tokenId);
+//             returnQuotes();
+//             started = false;
+//         }
+//     }
+// }
--- test/NFT/NFTAuction.t.sol
+++ test/NFT/NFTAuction.t.sol
@@ -1,84 +1,84 @@
-pragma solidity ^0.8.0;
+// pragma solidity ^0.8.0;
 
-import "src/NFT/myToken.sol";
-import "src/NFT/nftAuction.sol";
-import "foundry-huff/HuffDeployer.sol";
-import "forge-std/Test.sol";
-import "forge-std/console.sol";
+// import "src/NFT/myToken.sol";
+// import "src/NFT/nftAuction.sol";
+// import "foundry-huff/HuffDeployer.sol";
+// import "forge-std/Test.sol";
+// import "forge-std/console.sol";
 
-contract NFTAuctionTest is Test {
-    NFTAuction public nft;
-    MyERC721 public erc721;
-    uint public endingBid = 123;
-    uint public startingPrice = 100;
-    uint public tokenId = 1;
-    address public owner = address(123);
+// contract NFTAuctionTest is Test {
+//     NFTAuction public nft;
+//     MyERC721 public erc721;
+//     uint public endingBid = 123;
+//     uint public startingPrice = 100;
+//     uint public tokenId = 1;
+//     address public owner = address(123);
 
-    function setUp() public {
-        // Deploy NFT and ERC721 contracts for testing
-        vm.startPrank(owner);
-        nft = new  NFTAuction();
-        string memory name = "yael";
-        string memory symbole = "rachel";
-        erc721 = new MyERC721(name, symbole);
-        erc721.mint(owner, tokenId);
-        erc721.approve(address(nft), tokenId);
-    }
+//     function setUp() public {
+//         // Deploy NFT and ERC721 contracts for testing
+//         vm.startPrank(owner);
+//         nft = new  NFTAuction();
+//         string memory name = "yael";
+//         string memory symbole = "rachel";
+//         erc721 = new MyERC721(name, symbole);
+//         erc721.mint(owner, tokenId);
+//         erc721.approve(address(nft), tokenId);
+//     }
 
-    // Test function to check the start of the auction
-    function testStartAuction() public {
-        nft.start(address(erc721), endingBid, startingPrice, tokenId);
-        // Assert the state after calling start
-        assertEq(nft.endingBid(), endingBid);
-        assertEq(nft.startingPrice(), startingPrice);
-        assertEq(nft.tokenId(), tokenId);
-        assertEq(nft.started(), true);
-        assertEq(erc721.ownerOf(tokenId), address(nft));
-    }
+//     // Test function to check the start of the auction
+//     function testStartAuction() public {
+//         nft.start(address(erc721), endingBid, startingPrice, tokenId);
+//         // Assert the state after calling start
+//         assertEq(nft.endingBid(), endingBid);
+//         assertEq(nft.startingPrice(), startingPrice);
+//         assertEq(nft.tokenId(), tokenId);
+//         assertEq(nft.started(), true);
+//         assertEq(erc721.ownerOf(tokenId), address(nft));
+//     }
 
-    // Test function to add a quote to the auction
-    function testAddQuote() public {
-        address addr = vm.addr(12345);
-        vm.startPrank(addr);
-        vm.deal(address(addr), 5000);
-        uint256 quoteAmount = 150; // Higher than the starting price
-        nft.addQuote{value: quoteAmount}();
-        // Assert the state after adding a quote
-        (address maxStackAddress, uint256 maxStackAmount) = nft.getMaxStack();
-        assertEq(maxStackAddress, address(addr), "Unexpected maxStackAddress");
-        assertEq(maxStackAmount, quoteAmount, "Unexpected maxStackAmount");
-    }
+//     // Test function to add a quote to the auction
+//     function testAddQuote() public {
+//         address addr = vm.addr(12345);
+//         vm.startPrank(addr);
+//         vm.deal(address(addr), 5000);
+//         uint256 quoteAmount = 150; // Higher than the starting price
+//         nft.addQuote{value: quoteAmount}();
+//         // Assert the state after adding a quote
+//         (address maxStackAddress, uint256 maxStackAmount) = nft.getMaxStack();
+//         assertEq(maxStackAddress, address(addr), "Unexpected maxStackAddress");
+//         assertEq(maxStackAmount, quoteAmount, "Unexpected maxStackAmount");
+//     }
 
-    // Test function to cancel a quote from the auction
-    function testCancelQuote() public {
-        address addr = vm.addr(12345);
-        vm.startPrank(addr);
-        vm.deal(address(addr), 5000);
-        uint256 quoteAmount = 150; // Higher than the starting price
-        nft.addQuote{value: quoteAmount}();
-        nft.cancelQuote();
-        // Assert the state after canceling the quote
-        assert(!nft.getIsExist());
-    }
+//     // Test function to cancel a quote from the auction
+//     function testCancelQuote() public {
+//         address addr = vm.addr(12345);
+//         vm.startPrank(addr);
+//         vm.deal(address(addr), 5000);
+//         uint256 quoteAmount = 150; // Higher than the starting price
+//         nft.addQuote{value: quoteAmount}();
+//         nft.cancelQuote();
+//         // Assert the state after canceling the quote
+//         assert(!nft.getIsExist());
+//     }
 
-    // Test function to end the auction
-    function testEndAuction() public {
-        nft.start(address(erc721), endingBid, startingPrice, tokenId);
-        address addr = vm.addr(12345);
-        vm.startPrank(addr);
-        vm.deal(address(addr), 5000);
-        uint256 quoteAmount = 150; // Higher than the starting price
-        nft.addQuote{value: quoteAmount}();
-        vm.warp(124); // Fast forward time to simulate end of auction
-        vm.stopPrank();
-        vm.startPrank(owner);
-        nft.end();
-        // Assert the state after ending the auction
-        assertEq(nft.started(), false, "Auction is still active after ending");
-        assertEq(erc721.ownerOf(tokenId), address(addr), "Unexpected token owner after ending auction");
-        assertEq(address(owner).balance, 150, "Incorrect balance after ending auction");
-    }
-}
+//     // Test function to end the auction
+//     function testEndAuction() public {
+//         nft.start(address(erc721), endingBid, startingPrice, tokenId);
+//         address addr = vm.addr(12345);
+//         vm.startPrank(addr);
+//         vm.deal(address(addr), 5000);
+//         uint256 quoteAmount = 150; // Higher than the starting price
+//         nft.addQuote{value: quoteAmount}();
+//         vm.warp(124); // Fast forward time to simulate end of auction
+//         vm.stopPrank();
+//         vm.startPrank(owner);
+//         nft.end();
+//         // Assert the state after ending the auction
+//         assertEq(nft.started(), false, "Auction is still active after ending");
+//         assertEq(erc721.ownerOf(tokenId), address(addr), "Unexpected token owner after ending auction");
+//         assertEq(address(owner).balance, 150, "Incorrect balance after ending auction");
+//     }
+// }
 
 
 
