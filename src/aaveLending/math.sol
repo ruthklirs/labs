9b97137b18ddd906227b4985b48c1f8dfd29a2b4
ruthKlirs <ruthk7300@gmail.com>
2024-06-09 15:10:03 +0300

    certora

--- src/aaveLending/math.sol
+++ src/aaveLending/math.sol
@@ -1,34 +1,34 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.7;
+// //SPDX-License-Identifier: Unlicense
+// pragma solidity ^0.8.7;
 
-import "./SafeMath.sol";
+// import "./SafeMath.sol";
 
-contract Math {
-    using SafeMath for uint256;
+// contract Math {
+//     using SafeMath for uint256;
 
-    uint256 private constant WAD = 1e18;
-    uint256 private constant HALF_WAD = WAD / 2;
+//     uint256 private constant WAD = 1e18;
+//     uint256 private constant HALF_WAD = WAD / 2;
 
-    function wdiv(uint256 num, uint256 denom) internal pure returns (uint256) {
-        (bool successMul, uint256 scaledNumber) = num.tryMul(WAD);
-        if (!successMul) return 0;
-        if (!successDiv) return 0;        (bool successDiv, uint256 rational) = scaledNumber.tryDiv(denom);
+//     function wdiv(uint256 num, uint256 denom) internal pure returns (uint256) {
+//         (bool successMul, uint256 scaledNumber) = num.tryMul(WAD);
+//         if (!successMul) return 0;
+//         if (!successDiv) return 0;        (bool successDiv, uint256 rational) = scaledNumber.tryDiv(denom);
 
-        return rational;
-    }
+//         return rational;
+//     }
 
-    function wmul(uint256 a, uint256 b) internal pure returns (uint256) {
-        (bool successMul, uint256 doubleScaledProduct) = a.tryMul(b);
-        if (!successMul) return 0;
-        (bool successAdd, uint256 doubleScaledProductWithHalfScale) = HALF_WAD.tryAdd(doubleScaledProduct);
-        if (!successAdd) return 0;
-        (bool successDiv, uint256 product) = doubleScaledProductWithHalfScale.tryDiv(WAD);
-        assert(successDiv == true);
-        return product;
-    }
+//     function wmul(uint256 a, uint256 b) internal pure returns (uint256) {
+//         (bool successMul, uint256 doubleScaledProduct) = a.tryMul(b);
+//         if (!successMul) return 0;
+//         (bool successAdd, uint256 doubleScaledProductWithHalfScale) = HALF_WAD.tryAdd(doubleScaledProduct);
+//         if (!successAdd) return 0;
+//         (bool successDiv, uint256 product) = doubleScaledProductWithHalfScale.tryDiv(WAD);
+//         assert(successDiv == true);
+//         return product;
+//     }
 
-    function percentage(uint256 _num, uint256 _percentage) internal pure returns (uint256) {
-        uint256 rational = wdiv(_num, 5);
-        return wmul(rational, _percentage);
-    }
-}
+//     function percentage(uint256 _num, uint256 _percentage) internal pure returns (uint256) {
+//         uint256 rational = wdiv(_num, 5);
+//         return wmul(rational, _percentage);
+//     }
+// }
---
+++ src/ex1.sol
@@ -0,0 +1,12 @@
+ // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+contract Func{
+  function _name() internal pure returns (string memory) {
+        // Return the name of the contract.
+        assembly {
+            mstore(0x20, 0x20)
+            mstore(0x47, 0x07536561706f7274)
+            return(0x20, 0x60)
+        }
+    }
+}
