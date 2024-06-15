8154801c224b8f545a6f05a63c1a21251f58b2de
ruthKlirs <ruthk7300@gmail.com>
2024-06-16 00:35:58 +0300

    continue solidity by exemple

--- src/solidity_by_example/array.sol
+++ src/solidity_by_example/array.sol
@@ -6,13 +6,29 @@ contract Array {
     uint256[] public initializedArray = [1, 2, 3, 4];
     int256[5] public certainSizeArray;
 
-    function get(uint256 i) public view returns(int256){
+    function get(uint256 i) public view returns (int256) {
         return arr[i];
     }
 
-    
-    function getArr() external view returns(int256[] memory){
+    function getArr() external view returns (int256[] memory) {
         return arr;
-        
+    }
+
+    function push(int256 i) public {
+        arr.push(i);
+    }
+
+    function pop() public {
+        arr.pop();
+    }
+
+    function remove(uint256 i) public {
+        delete arr[i];
+    }
+    function getLengthArr() public view returns(uint256){
+        return arr.length;
+    }
+    function memoryArray() external{
+        uint256[] memory tempArr=new uint256[](5);
     }
 }
